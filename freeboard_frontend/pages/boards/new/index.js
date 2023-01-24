8c500c9db709dc5d33773cb655a2ad78313bbf9f
Aria(김인숙) <kis@gabia.com>
2023-01-25 08:35:42 +0900

    update: emotion 연습

--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -1,4 +1,4 @@
-import { Wrapper, Title, Label, RegistButton } from '../../../styles/freeboard'
+import { Wrapper, Title, Label, FormWrapper, FormInput, FormContent, RegistButton } from '../../../styles/freeboard'
 
 export default function FreeboardNew() {
   return(
@@ -6,40 +6,51 @@ export default function FreeboardNew() {
       <Wrapper>
         <Title>게시물 등록</Title>
         <div>
-          <Label>작성자</Label>
-          <input type="text" placeholder='이름을 적어주세요.' />
+          <FormWrapper>
+            <Label>작성자</Label>
+            <FormInput type="text" placeholder='이름을 적어주세요.' />
+          </FormWrapper>
+          <FormWrapper>
+            <Label>비밀번호</Label>
+            <FormInput type="password" placeholder='비밀번호를 입력해주세요.' />
+          </FormWrapper>
         </div>
-        <div>
-          <Label>비밀번호</Label>
-          <input type="password" />
-        </div>
-        <div>
+        <FormWrapper>
           <Label>제목</Label>
-          <input type="text" />
-        </div>
-        <div>
+          <FormInput type="text" placeholder='제목을 작성해주세요.' />
+        </FormWrapper>
+        <FormWrapper>
+          <Label>내용</Label>
+          <FormContent placeholder='내용을 작성해주세요.' />
+        </FormWrapper>
+        <FormWrapper>
           <Label>주소</Label>
-          <input type="text" />
+          <FormInput type="text" />
           <button>우편번호 검색</button>
           <div>
-            <input type="text" />
+            <FormInput type="text" />
           </div>
           <div>
-            <input type="text" />
+            <FormInput type="text" />
           </div>
-        </div>
-        <div>
+        </FormWrapper>
+        <FormWrapper>
           <Label>유튜브</Label>
-          <input type="text" />
-        </div>
-        <div>
+          <FormInput type="text" placeholder='링크를 복사해주세요.' />
+        </FormWrapper>
+        <FormWrapper>
           <Label>사진 첨부</Label>
-          <input type="text" />
-        </div>
-        <div>
+          
+        </FormWrapper>
+        <FormWrapper>
           <Label>메인 설정</Label>
-          <input type="text" />
-        </div>
+          <label htmlFor="youtube">
+            <input type="radio" id='youtube' checked name="main" /> 유튜브
+          </label>
+          <label htmlFor="pic">
+            <input type="radio" id='pic' name="main" /> 사진
+          </label>
+        </FormWrapper>
         <RegistButton type="button">등록하기</RegistButton>
       </Wrapper>
       
--- freeboard_frontend/styles/freeboard.js
+++ freeboard_frontend/styles/freeboard.js
@@ -1,24 +1,50 @@
 import styled from '@emotion/styled'
 
 export const Wrapper = styled.div`
-  width: 1200px;
-  margin: 0 auto;
+  //width: 1200px;
+  box-sizing: border-box;
+  margin: 100px auto;
   padding: 60px 102px 100px;
   box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
   font-family: 'Noto Sans CJK KR';
 `
-
 export const Title = styled.h1`
   margin: 0;
   padding: 0;
   text-align: center;
   font-size: 36px;
+  box-sizing: border-box;
 `
-
 export const Label = styled.h2`
   margin: 0;
-  padding: 0;
+  padding: 0 0 16px;
   font-size: 16px;
+  box-sizing: border-box;
+`
+export const FormWrapper = styled.div`
+  padding: 40px 0 0 0;
+  box-sizing: border-box;
+`
+export const FormInput = styled.input`
+  border: 1px solid #bdbdbd;
+  width: 100%;
+  height: 52px;
+  padding: 0 0 0 16px;
+  box-sizing: border-box;
+
+  &::placeholder {
+    color: #bdbdbd;
+  }
+`
+export const FormContent = styled.textarea`
+  border: 1px solid #bdbdbd;
+  width: 100%;
+  height: 480px;
+  padding: 16px 0 0 14px;
+  box-sizing: border-box;
+  &::placeholder {
+    color: #bdbdbd;
+  }
 `
 export const RegistButton = styled.button`
   display: block;
@@ -29,4 +55,5 @@ export const RegistButton = styled.button`
   text-align: center;
   background: #FFD600;
   cursor: pointer;
+  box-sizing: border-box;
 `
