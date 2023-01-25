070c47ea78c832b66f5385027224f45aa2c71716
Aria Kim <shoo7830@gmail.com>
2023-01-25 23:52:21 +0900

    update: emotion 스타일링

--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -1,20 +1,20 @@
-import { Wrapper, Title, Label, FormWrapper, FormInput, FormContent, RegistButton } from '../../../styles/freeboard'
+import { Wrapper, Title, GridWrapper, Label, FormWrapper, ZipcodeWrapper, FormInput, FormContent, RegistButton } from '../../../styles/freeboard'
 
 export default function FreeboardNew() {
   return(
     <>
       <Wrapper>
         <Title>게시물 등록</Title>
-        <div>
-          <FormWrapper>
+        <GridWrapper>
+          <div>
             <Label>작성자</Label>
             <FormInput type="text" placeholder='이름을 적어주세요.' />
-          </FormWrapper>
-          <FormWrapper>
+          </div>
+          <div>
             <Label>비밀번호</Label>
             <FormInput type="password" placeholder='비밀번호를 입력해주세요.' />
-          </FormWrapper>
-        </div>
+          </div>
+        </GridWrapper>
         <FormWrapper>
           <Label>제목</Label>
           <FormInput type="text" placeholder='제목을 작성해주세요.' />
@@ -25,14 +25,12 @@ export default function FreeboardNew() {
         </FormWrapper>
         <FormWrapper>
           <Label>주소</Label>
-          <FormInput type="text" />
-          <button>우편번호 검색</button>
-          <div>
+          <ZipcodeWrapper>
             <FormInput type="text" />
-          </div>
-          <div>
+            <button>우편번호 검색</button>
             <FormInput type="text" />
-          </div>
+            <FormInput type="text" />
+          </ZipcodeWrapper>
         </FormWrapper>
         <FormWrapper>
           <Label>유튜브</Label>
@@ -40,12 +38,31 @@ export default function FreeboardNew() {
         </FormWrapper>
         <FormWrapper>
           <Label>사진 첨부</Label>
-          
+          <ul>
+            <li>
+              <button>
+                +<br />
+                Upload
+              </button>
+            </li>
+            <li>
+              <button>
+                +<br />
+                Upload
+              </button>
+            </li>
+            <li>
+              <button>
+                +<br />
+                Upload
+              </button>
+            </li>
+          </ul>
         </FormWrapper>
         <FormWrapper>
           <Label>메인 설정</Label>
           <label htmlFor="youtube">
-            <input type="radio" id='youtube' checked name="main" /> 유튜브
+            <input type="radio" id='youtube' name="main" /> 유튜브
           </label>
           <label htmlFor="pic">
             <input type="radio" id='pic' name="main" /> 사진
--- freeboard_frontend/styles/freeboard.js
+++ freeboard_frontend/styles/freeboard.js
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled'
 
 export const Wrapper = styled.div`
-  //width: 1200px;
+  width: 1200px;
   box-sizing: border-box;
   margin: 100px auto;
   padding: 60px 102px 100px;
-  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
-  font-family: 'Noto Sans CJK KR';
+  box-shadow: 0px 0 20px rgba(0, 0, 0, 0.2);
+  font-family: Arial, Helvetica, sans-serif;
 `
 export const Title = styled.h1`
   margin: 0;
@@ -15,15 +15,80 @@ export const Title = styled.h1`
   font-size: 36px;
   box-sizing: border-box;
 `
+export const GridWrapper = styled.div`
+  padding-top: 120px;
+  display: grid;
+
+  div & {
+    grid-template-columns: 1fr 1fr;
+    column-gap: 24px;
+  }
+`
+export const FormWrapper = styled.div`
+  padding: 40px 0 0 0;
+  box-sizing: border-box;
+
+  & input[type='radio'] {
+    accent-color: #FFD600;
+  }
+
+  & label + label{
+    margin-left: 20px;
+  }
+
+  & ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+  }
+
+  & li {
+    display: inline-flex;
+    margin-right: 24px;
+
+    & button {
+      margin: 0;
+      padding: 0;
+      width: 78px;
+      height: 78px;
+      border: 0;
+      background: #BDBDBD;
+      color: #4F4F4F;
+      cursor: pointer;
+    }
+  }
+`
 export const Label = styled.h2`
   margin: 0;
   padding: 0 0 16px;
   font-size: 16px;
   box-sizing: border-box;
 `
-export const FormWrapper = styled.div`
-  padding: 40px 0 0 0;
-  box-sizing: border-box;
+export const ZipcodeWrapper = styled.div`
+  & input:nth-of-type(1) {
+    width: 77px;
+  }
+
+  & input:nth-of-type(2) {
+    margin-top: 16px;
+  }
+
+  & input:nth-of-type(3) {
+    margin-top: 30px;
+  }
+
+  & button {
+    margin: 0 0 0 16px;
+    padding: 0;
+    border: 0;
+    background: #000;
+    color: #fff;
+    text-align: center;
+    height: 52px;
+    width: 124px;
+    cursor: pointer;
+  }
 `
 export const FormInput = styled.input`
   border: 1px solid #bdbdbd;
@@ -42,13 +107,14 @@ export const FormContent = styled.textarea`
   height: 480px;
   padding: 16px 0 0 14px;
   box-sizing: border-box;
+  resize: none;
   &::placeholder {
     color: #bdbdbd;
   }
 `
 export const RegistButton = styled.button`
   display: block;
-  margin: 0 auto;
+  margin: 80px auto 0;
   border: 0;
   width: 170px;
   padding: 14px 0;
