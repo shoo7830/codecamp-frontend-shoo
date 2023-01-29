2a93692f2dd2ceac4ad84930128959acc71cda79
Aria Kim <shoo7830@gmail.com>
2023-01-29 17:10:44 +0900

    add: quiz 풀기

--- class_quiz/pages/01/index.js
+++ class_quiz/pages/01/index.js
@@ -1,8 +1,6 @@
-import { Wrapper, Header, Search, InfoWrapper, Title, NameWrapper, MenuList, MenuItem, FaqList } from '../../styles/emotion'
+import { Wrapper, Header, Search, InfoWrapper, Title, NameWrapper, Name, MenuList, MenuItem, FaqList, FaqItem, FaqTitle, FaqNumber, FaqButton, FooterMenu, FooterItem } from '../../styles/emotion'
 
 const MyPages = () => {
-  
-  
   return (
     <Wrapper>
       <Header>
@@ -12,7 +10,11 @@ const MyPages = () => {
             마이
           </Title>
           <NameWrapper>
-            
+            <img src="/thumbnail.png" alt="" />
+            <Name>
+              임정아 
+              <img src="/arrow-right.svg" alt="" />
+            </Name>
           </NameWrapper>
         </InfoWrapper>
         <MenuList>
@@ -23,8 +25,76 @@ const MyPages = () => {
         </MenuList>
       </Header>
       <FaqList>
-
+        
+        <FaqItem>
+          <FaqTitle>
+            <FaqNumber>Q.01</FaqNumber>
+            리뷰 작성은 어떻게 하나요?
+          </FaqTitle>
+          <FaqButton>
+            <img src="/arrow-down.svg" alt="" />
+          </FaqButton>
+        </FaqItem>
+        <FaqItem>
+          <FaqTitle>
+            <FaqNumber>Q.02</FaqNumber>
+            리뷰 수정/삭제는 어떻게 하나요?
+          </FaqTitle>
+          <FaqButton>
+            <img src="/arrow-down.svg" alt="" />
+          </FaqButton>
+        </FaqItem>
+        <FaqItem>
+          <FaqTitle>
+            <FaqNumber>Q.03</FaqNumber>
+            아이디/비밀번호를 잊어버렸어요
+          </FaqTitle>
+          <FaqButton>
+            <img src="/arrow-down.svg" alt="" />
+          </FaqButton>
+        </FaqItem>
+        <FaqItem>
+          <FaqTitle>
+            <FaqNumber>Q.04</FaqNumber>
+            회원탈퇴를 하고싶어요.
+          </FaqTitle>
+          <FaqButton>
+            <img src="/arrow-down.svg" alt="" />
+          </FaqButton>
+        </FaqItem>
+        <FaqItem>
+          <FaqTitle>
+            <FaqNumber>Q.05</FaqNumber>
+            출발지 설정은 어떻게 하나요?
+          </FaqTitle>
+          <FaqButton>
+            <img src="/arrow-down.svg" alt="" />
+          </FaqButton>
+        </FaqItem>
+        <FaqItem>
+          <FaqTitle>
+            <FaqNumber>Q.06</FaqNumber>
+            비밀번호를 변경하고 싶어요
+          </FaqTitle>
+          <FaqButton>
+            <img src="/arrow-down.svg" alt="" />
+          </FaqButton>
+        </FaqItem>
       </FaqList>
+      <FooterMenu>
+        <FooterItem>
+          <img src="/home.svg" alt="" /><p>홈</p>
+        </FooterItem>
+        <FooterItem>
+          <img src="/pin.svg" alt="" /><p>잇츠로드</p>
+        </FooterItem>
+        <FooterItem>
+          <img src="/pick.svg" alt="" /><p>마이찜</p>
+        </FooterItem>
+        <FooterItem>
+          <img src="/my.svg" alt="" /><p>마이</p>
+        </FooterItem>
+      </FooterMenu>
     </Wrapper>
   )
 }
--- class_quiz/styles/emotion.js
+++ class_quiz/styles/emotion.js
@@ -11,6 +11,7 @@ export const Search = styled.div`
 `
 export const InfoWrapper = styled.div`
   display: flex;
+  align-items: center;
   margin-top: 32px;
 `
 export const Title = styled.h1`
@@ -19,13 +20,30 @@ export const Title = styled.h1`
   font-size: 40px;
 `
 export const NameWrapper = styled.div`
- justify-content: flex-end;
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  & img {
+    width: 60px;
+    height: auto;
+  }
+`
+export const Name = styled.a`
+  font-weight: bold;
+  font-size: 24px;
+  padding-left: 20px;
+  
+  & img {
+    width: 12px;
+    height: auto;
+    padding-left: 10px;
+  }
 `
 
 export const MenuList = styled.div`
   display: flex;
   justify-content: space-between;
-  margin: 43px 0 58px;
+  margin: 43px 0 0;
   padding: 0;
 `
 export const MenuItem = styled.a`
@@ -40,8 +58,58 @@ export const MenuItem = styled.a`
     color: #FF1B6D;
     border-bottom: 2px solid #FF1B6D;
   }
+
+  
 `
 export const FaqList = styled.div`
   border-top: 1px solid #CACACA ;
   border-bottom: 1px solid #CACACA ;
+  padding-left: 50px;
+  padding-right: 40px;
+`
+export const FaqItem = styled.div`
+  display: flex;
+  padding: 25px 0;
+`
+export const FaqTitle = styled.div`
+  margin: 0;
+  padding: 0;
+  font-size: 24px;
+`
+export const FaqNumber = styled.p`
+  margin: 0;
+  padding: 0 0 5px;
+  color: #ADADAD;
+  font-size: 18px;
+`
+export const FaqButton = styled.button`
+  margin: 0 13px 0 auto;
+  padding: 0;
+  border: 0;
+  background: transparent;
+`
+export const FooterMenu = styled.div`
+  display: flex;
+  align-items: flex-end;
+  height: 96px;
+  padding-bottom: 12px;
+  justify-content: space-around;
+  border-bottom: 1px solid #CACACA ;
+`
+export const FooterItem = styled.a`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  color: #ADADAD;
+  :nth-of-type(4) {
+    color: #FF1B6D;
+  }
+
+  & p {
+    margin: 0;
+    padding: 10px 0 0;
+    text-align: center;
+  }
 `
